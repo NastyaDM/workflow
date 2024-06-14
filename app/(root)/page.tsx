dfc4a892def8cccb3f61d6c16f4c2ff86f665723
NastyaDM <m3lody.star0100056@yandex.ru>
2024-06-15 06:16:00 +0700

    release

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -262,7 +262,7 @@ export default async function Home() {
               <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
                   <div className="w-20 h-20 relative rounded-full overflow-hidden">
-                    <Image src="/women_avatar.png" fill alt="" />
+                    <Image src="/women_avatar.jpg" fill alt="" />
                   </div>
                   <div>
                     <h6 className="text-xl font-medium text-primary">
@@ -289,7 +289,7 @@ export default async function Home() {
               <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
                   <div className="w-20 h-20 relative rounded-full overflow-hidden">
-                    <Image src="/man_avatar.png" fill alt="" />
+                    <Image src="/man_avatar.jpg" fill alt="" />
                   </div>
                   <div>
                     <h6 className="text-xl font-medium text-primary">
